b56871130f6d1f66d03601fb66b2272256df90e5
Hang <64747715+xx529@users.noreply.github.com>
2023-09-19 01:01:25 +0800

    update

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ from prompt import TranslatePrompt
 if __name__ == '__main__':
     m = ZhiPuAIModel(os.getenv('ZHIPUAI_API_KEY'), os.getenv('ZHIPUAI_MODEL'))
 
-    prompt = TranslatePrompt.prompt('英语', '法语', '今天星期几')
+    prompt = TranslatePrompt.prompt('中文', '英文', '今天星期几')
 
     print(m.chat(prompt))
--- prompt.py
+++ prompt.py
@@ -6,12 +6,12 @@ class TranslatePrompt:
     @classmethod
     def prompt(cls, src_lang, dst_lang, content):
         _prompt = f"""
-            请将以下{src_lang}句子翻译成{dst_lang}，你需要根据一下步骤一步一歩来
+            现在你需要将一段{src_lang}句子翻译成{dst_lang}，你需要根据一下步骤一步一歩来
             
             1. 先判断给出的句子是否{src_lang}，如果不是请回答："输入的句子不是{src_lang}"
             2. 请将其翻译成{dst_lang}，并回答："翻译结果：xxx"
             
-            以下是需要翻译的句子：
+            以下是句子：
             {content}
             """
         Log.info(_prompt)
